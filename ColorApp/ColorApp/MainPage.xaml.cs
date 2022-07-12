7ed42a64bd7324f4ae57a044301d21263438dd0a
nicole-whatley <nicole.whatley@mccroryschools.org>
2022-07-12 14:22:49 -0500

    First Commit

--- ColorApp/ColorApp/MainPage.xaml.cs
+++ ColorApp/ColorApp/MainPage.xaml.cs
@@ -2,23 +2,29 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void Button_Clicked(object sender, EventArgs e)
 	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+	private void MagentaClicked(object sender, EventArgs e)
+	{
+		cPage.BackgroundColor = Color.Parse("Magenta");
+	}
+
+	private void OrangeClicked(object sender, EventArgs e)
+	{
+        cPage.BackgroundColor = Color.Parse("Orange");
+    }
+
+	private void ResetClicked(object sender, EventArgs e)
+	{
+        cPage.BackgroundColor = Color.Parse("Black");
+    }
 }
 
